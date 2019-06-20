64326e75439d97d9d06f103dd312d7f44274ea7d
rachelstarry <rachel.starry@gmail.com>
2019-06-20 10:51:37 -0400

    added manual text processing and processed outputs

--- CHANGES.md
+++ CHANGES.md
@@ -18,9 +18,9 @@
 ## Processed Data
 
 ### .txt
-* "sample1911OriginalEncoding.txt" = cut out lines from "HerrickDiary1911-1915.txt" for Jan-Dec 1911 entries; realized text was encoded as Western, converted sample to UTF-8 using https://subtitletools.com/convert-text-files-to-utf8-online and saved as "sample1911.txt"
+* "sample1911OriginalEncoding.txt" = 2019-06-18 cut out lines from "HerrickDiary1911-1915.txt" for Jan-Dec 1911 entries; realized text was encoded as Western, converted sample to UTF-8 using https://subtitletools.com/convert-text-files-to-utf8-online and saved as "sample1911.txt"
 * "sample1911.txt" = see above
-* "sample1915OriginalEncoding.txt" = same process as above for Jan-Dec 1915 entries, saved UTF-8 version as "sample1915.txt"
+* "sample1915OriginalEncoding.txt" = 2019-06-18 same process as above for Jan-Dec 1915 entries, saved UTF-8 version as "sample1915.txt"
 * "sample1915.txt" = see above
 
 ### .csv (?)
--- processTranscripts.R
+++ processTranscripts.R
@@ -1,14 +1,15 @@
 library(tidyverse)
 library(quanteda)
+library(tokenizers)
 
-# SAMPLE TEXT PROCESSING ----
+# SAMPLE TEXT PROCESSING (quanteda) ----
 # Read in sample text files
 sample1 <- read_lines("data/sample1911.txt") %>% paste0(collapse = "")
 sample2 <- read_lines("data/sample1915.txt") %>% paste0(collapse = "")
 
 # Tokenize sample texts, removing numbers and punctuation
 s1_words <- tokens(sample1, remove_numbers = TRUE, remove_punct = TRUE, remove_hyphens = FALSE)
-s2_words <- tokens(sample1, remove_numbers = TRUE, remove_punct = TRUE, remove_hyphens = FALSE)
+s2_words <- tokens(sample2, remove_numbers = TRUE, remove_punct = TRUE, remove_hyphens = FALSE)
 
 s1_sentences <- tokens(sample1, remove_numbers = TRUE, what = "sentence")
 s2_sentences <- tokens(sample2, remove_numbers = TRUE, what = "sentence")
@@ -21,7 +22,7 @@ s2_ngrams <- tokens_ngrams(s2_words, n = 2:3)
 docs <- c(paste0(s1_words$text1, collapse=" "), paste0(s2_words$text1, collapse=" "))
 doc_term_matrix <- dfm(docs, stem = FALSE, tolower = TRUE, remove = stopwords())
 
-# SAMPLE CORPUS ANALYSES ----
+# SAMPLE CORPUS ANALYSIS (quanteda) ----
 # Simple frequency analysis
 feature_freq <- textstat_frequency(doc_term_matrix, n = 100)
 
@@ -33,6 +34,45 @@ keyness <- textstat_keyness(doc_term_matrix, target = doc_term_matrix@Dimnames$d
 textplot_keyness(keyness)
 
 # Collocation analysis
-proper_names <- tokens(sample1) %>% 
+proper_names1 <- tokens(sample1) %>% 
   tokens_select(pattern = '^[A-Z]', valuetype = 'regex', case_insensitive = FALSE, padding = TRUE) %>% 
   textstat_collocations(min_count = 4)
+proper_names2 <- tokens(sample2) %>% 
+  tokens_select(pattern = '^[A-Z]', valuetype = 'regex', case_insensitive = FALSE, padding = TRUE) %>% 
+  textstat_collocations(min_count = 4)
+
+# SAMPLE TEXT PROCESSING (manual) ----
+# Function to clean sample texts
+clean_sample <- function(text) {
+  # Remove blank lines
+  indexes <- which(text == "" | text == " ")
+  if (length(indexes) > 0) {
+    text <- text[-indexes]
+  }
+  
+  # Loop through remaining lines, performing additional cleanup
+  clean_text <- NULL
+  for (i in 1:length(text)) {
+    # Text cleanup
+    temp <- text[i] %>% 
+      tolower() %>% 
+      #str_replace_all("\\p{quotation mark}", "") %>%
+      str_replace_all("[:punct:]", "") %>% 
+      trimws()
+    
+    clean_text <- c(clean_text, temp)
+  }
+  return(clean_text)
+}
+
+# Read in, clean, and output sample texts
+sample3 <- read_lines("data/sample1911.txt")
+sample4 <- read_lines("data/sample1915.txt")
+
+sample3_clean <- clean_sample(sample3) %>% paste0(collapse = " ") %>% str_replace_all("\\s+", " ")
+write_lines(sample3_clean, "data/sample1911_processed.txt")
+
+sample4_clean <- clean_sample(sample4) %>% paste0(collapse = " ") %>% str_replace_all("\\s+", " ")
+write_lines(sample4_clean, "data/sample1915_processed.txt")
+
+# SAMPLE TEXT PROCESSING (tokenizers) ----
